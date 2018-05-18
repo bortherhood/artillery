910fc3881526a207a02a36171a8836af94fee7c3
Lone_Wolf <lonewolf04361@gmail.com>
2018-05-18 16:29:11 -0700

    Add files via upload

--- heavy.lua
+++ heavy.lua
@@ -1,63 +1,77 @@
-function kaboom(pos, radius)
-		tnt.boom(pos, {radius = radius, damage_radius = radius})
-end
--- Shell entity
-local shell_ent = {
-	physical = true,
-	timer = 10,
-	visual = "cube",
-	visual_size = {x=0.2, y=0.2,},
-	textures = {'default_stone.png', 'default_stone.png', 'default_stone.png', 'default_stone.png', 'default_stone.png', 'default_stone.png'},
-	collisionbox = {1, 1, 1, 1, 1, 1},
-}
-
---Registering the entity
-minetest.register_entity("artillery:shellent", shell_ent)
-
---This is where the cool stuff starts
-function fire_bomb(name, radius, pos, distance, direction)
-
-  	local dir = minetest.facedir_to_dir(direction)
-	local obj = minetest.add_entity({x=pos.x+dir.x, y=pos.y+1, z=pos.z+dir.z}, name) --Spawn bomb
-
-	obj:set_velocity({x=dir.x*(distance/2), y=20, z=dir.z*(distance/2)}) --Starting velocity
-	obj:setacceleration({x=dir.x*(distance/2), y=-25, z=dir.z*(distance/2)}) --Acceleration after the bomb starts moving
-
-	shell_ent.on_step = function(self, dtime)
-	self.timer = self.timer + dtime
-	local pos = self.object:getpos()
-
-	if self.timer > 0.12 then
-		local objs = minetest.get_objects_inside_radius({x = pos.x, y = pos.y, z = pos.z}, 1)
-		for k, obj in pairs(objs) do
-			if obj:get_luaentity() ~= nil then
-				if obj:get_luaentity().name ~= "artillery:shellent" and obj:get_luaentity().name ~= "__builtin:item" then
-					kaboom(obj:get_pos(), radius)
-					self.object:remove()
-				end
+local function check(obj, pos)
+	local node = minetest.get_node(pos)
+	if node.name ~= "air" and node.name ~= "artillery:barbed_wire" then
+		if node.name == "artillery:concrete" then
+			obj.object:remove()
+			tnt.boom(pos, {radius = 2, damage_radius = 2})
+			elseif node.name == "artillery:concrete_cracked_1" or 
+				node.name == "artillery:concrete_cracked_2" or 
+				node.name == "artillery:concrete_cracked_3" then
+				obj.object:remove()
+				tnt.boom(pos, {radius = 2, damage_radius = 3})
 			else
-				kaboom(obj:get_pos(), radius)
-				self.object:remove()
-			end
-
---Prevent bomb from exploding when inside air and removing it when it hits water
-			local node = minetest.get_node(obj:get_pos())
-			if minetest.get_node(obj:get_pos()) then
-				if node.name == "default:water_source" or node.name == "default:water_flowing" then
-					self.object:remove()
-						elseif node.name == "air" then
-							return
-						else
-							kaboom(obj:get_pos(), radius)
-							self.object:remove()
-							end
+				obj.object:remove()
+				tnt.boom(pos, {radius = 3, damage_radius = 5})
 		end
 	end
 end
-end
+
+local time_since_spawned = 0
+
+local bomb_entity = {
+	physical = true,
+	timer = 0,
+	visual = "cube",
+	visual_size = {x=0.2, y=0.2,},
+	textures = {'default_stone.png', 'default_stone.png', 'default_stone.png', 'default_stone.png', 'default_stone.png', 'default_stone.png'},
+	collisionbox = {4, 4, 4, 4, 4, 4},
+	on_step = function(self, dtime)
+		local pos = self.object:getpos()
+		check(self, pos)
+	end
+}
+
+local grenade_entity = {
+	physical = true,
+	timer = 0,
+	visual = "mesh",
+	mesh = "gwenade.obj",
+	visual_size = {x=1.5, y=1.5, z=1.5},
+	textures = {'default_stone.png'},
+	collisionbox = {10, 10, 10, 10, 10, 10},
+	on_step = function(self, dtime)
+		local pos = self.object:getpos()
+		local node = minetest.get_node(pos)
+		time_since_spawned = time_since_spawned + dtime
+
+		if time_since_spawned > 4.5 or node.name ~= "air" then
+			time_since_spawned = 0
+			tnt.boom({x=pos.x, y=pos.y+1.2, z=pos.z}, {radius = 1, damage_radius = 6})
+			self.object:remove()
+		end
+	end
+}
+
+minetest.register_entity("artillery:bomb", bomb_entity)
+minetest.register_entity("artillery:grenadey", grenade_entity)
+
+function fire_bomb(name, radius, pos, distance, direction)
+	local dir = minetest.facedir_to_dir(direction)
+	local obj = minetest.add_entity({x=pos.x+dir.x, y=pos.y+1, z=pos.z+dir.z}, name) --Adds bomb one node above starting node
+
+	obj:set_velocity({x=dir.x*(distance/2), y=20, z=dir.z*(distance/2)}) --Starting velocity
+	obj:setacceleration({x=dir.x*(distance/2), y=-30, z=dir.z*(distance/2)}) --Acceleration after the bomb starts moving. Negative numbers slow it down
 end
 
-minetest.register_craftitem("artillery:shell", { --kind of like a turtle shell with high explosive and phosphorus
+function throw_grenade(name, pos, direction)
+	local dir = direction
+	local obj = minetest.add_entity({x=pos.x+dir.x, y=pos.y+1.3, z=pos.z+dir.z}, name)
+
+	obj:set_velocity({x=dir.x*5, y=5, z=dir.z*5}) --Starting velocity
+	obj:setacceleration({x=dir.x, y=-10, z=dir.z}) --Acceleration after the bomb starts moving
+end
+
+minetest.register_craftitem("artillery:shell", {
 	description = "Explosive Shell",
 	image = "shell.png",
 })
@@ -69,6 +83,7 @@ minetest.register_node("artillery:mortar", {
 	groups = {oddly_breakable_by_hand = 3, attached_node = 1, force_floor = 1},
 	mesh = "mortar.obj",
 	paramtype2 = "facedir",
+	force_floor,
 	selection_box = {
 		type = "fixed",
 		fixed = {0.6, -0.3, 0.6, -0.6, -0.5, -0.6},
@@ -92,36 +107,35 @@ minetest.register_node("artillery:mortar", {
 
 	on_receive_fields = function(pos, formname, fields, player)
 	local meta = minetest.get_meta(pos)
-    	if fields.save and fields.distance ~= "" and fields.height ~= "" and fields.direction ~= "" then
-    		--Prevent user from putting in disallowed characters or values
-
+		if fields.save and fields.distance ~= "" then
+			--Prevent user from putting in disallowed characters or values
 			local function limity(input, limit)
-    	    	local inputy = tonumber(input)
-    	    	if inputy == nil then
-    	    		return(0)
-    	    	end
-    	    	if inputy > limit then
-    	    		return(limit)
-    	    		elseif inputy < 5 then
-    	    			return(5)
-    	    		else
-    	    			return(inputy)
-    	    	end
-    	    end
+				local inputy = tonumber(input)
+				if inputy == nil then
+					return(5)
+				end
+				if inputy > limit then
+					return(limit)
+					elseif inputy < 5 then
+						return(5)
+					else
+						return(inputy)
+				end
+			end
 
-    	    meta:set_int("distance", limity(fields.distance, 40))
-    	    meta:set_string("formspec",
+			meta:set_int("distance", limity(fields.distance, 40))
+			meta:set_string("formspec",
 				"size[3,2.2]" ..
 				"bgcolor[#080808BB;true]" ..
 				"background[5,5;1,1;gui_formbg.png;true]" ..
 				"label[0,-0.3;Mortar Configuration]" ..
 				"field[0.5,1;2.5,1;distance; Power(distance);"..meta:get_int("distance").."]" ..
 				"button_exit[0,1.5;3,1;save;Save Configuration]")
-    	end
+		end
 	end,
 
 	on_punch = function(pos, node, puncher, pointed_thing)
-	local node = minetest.get_node(pos)
+		local nodey = minetest.get_node(pos)
 		local inv = puncher:get_inventory()
 		local itemstack = puncher:get_wielded_item()
 		local meta = minetest.get_meta(pos)
@@ -129,10 +143,10 @@ minetest.register_node("artillery:mortar", {
 			if itemstack:get_name() ~= "artillery:shell" then
 				return itemstack
 			end
-			if not minetest.setting_getbool("creative_mode") then --Won't lose the Explosive Shell if you are in creative mode
+			if not minetest.setting_getbool("creative_mode") then
 				inv:remove_item("main", "artillery:shell")
 			end
-			fire_bomb("artillery:shellent", 5, pos, meta:get_int("distance"), node.param2)
+			fire_bomb("artillery:bomb", 5, pos, meta:get_int("distance"), nodey.param2)
 			meta:set_int("cooldown", 1)
 			meta:set_string("infotext", "Mortar Launcher is cooling down")
 			minetest.after(2.5, function()
@@ -142,3 +156,54 @@ minetest.register_node("artillery:mortar", {
 		end
 	end,
 })
+
+
+--Grenade
+
+minetest.register_node("artillery:grenade", {
+	drawtype = "mesh",
+	mesh = "gwenade.obj",
+	description = "Grenade (Right-Click to pull pin, Left-Click to throw",
+	tiles = "default_stone.png^default_diamond.png",--"grenade.png",
+	stack_max = 1,
+	range = 0,
+	on_secondary_use = function(itemstack, user, pointed_thing)
+		local player = user:get_player_name()
+		local meta = itemstack:get_meta()
+		local inv = user:get_inventory()
+
+		if player ~= nil and meta:get_int("active") == 0 then
+			meta:set_int("active", 1)
+			minetest.after(5, function()
+				if inv:contains_item("main", "artillery:grenade") then
+					tnt.boom(user:get_pos(), {radius = 1, damage_radius = 4})
+					if not minetest.setting_getbool("creative_mode") then
+						inv:remove_item("main", "artillery:grenade")
+					end
+				end
+			end)
+			return itemstack
+		end
+	end,
+	on_use = function(itemstack, placer, pointed_thing)
+		local player = placer:get_player_name()
+		local meta = itemstack:get_meta()
+		local inv = placer:get_inventory()
+
+		if player ~= nil and meta:get_int("active") == 1 then
+			throw_grenade("artillery:grenadey", placer:get_pos(), placer:get_look_dir())
+			if not minetest.setting_getbool("creative_mode") then
+				inv:remove_item("main", "artillery:grenade")
+			end
+		end
+	end,
+})
+
+minetest.register_craft({
+	output = "artillery:grenade",
+	recipe = {
+	{"", "default:steel_ingot", ""},
+	{"default:steel_ingot", "tnt:tnt", "default:steel_ingot"},
+	{"", "default:steel_ingot", ""}
+	},
+	})
