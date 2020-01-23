ccdf3d0f4fe88b575e14e2714545b71da8da33b6
LoneWolfHT <lonewolf04361@gmail.com>
2020-01-22 17:01:28 -0800

    Convert to modpack

---
+++ artillery_guns/init.lua
@@ -0,0 +1,3 @@
+dofile(minetest.get_modpath("artillery_guns") .. "/heavy.lua")
+
+minetest.log("[ARTILLERY:GUNS] Loaded Succesfully.")
---
+++ artillery_turrets/init.lua
@@ -0,0 +1,1 @@
+minetest.log("[ARTILLERY:TURRETS] Loaded Succesfully.")
